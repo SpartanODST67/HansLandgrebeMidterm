12abd579307bffacca96592000fa053fcd1151f4
Hans Landgrebe <hlxbl20@gmail.com>
2024-02-07 12:08:25 -0600

    Implemented UI

--- HansLandgrebe/Assets/Scripts/ScoreKeeper.cs
+++ HansLandgrebe/Assets/Scripts/ScoreKeeper.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreKeeper : MonoBehaviour
 {
     [SerializeField] AudioSource scoreSound;
-    [SerializeField] int score;
+    [SerializeField] TextMeshProUGUI scoreText;
+    private int score;
 
     public void addScore(int score)
     {
         this.score += score;
+        updateScoreText();
+    }
+
+    public void updateScoreText()
+    {
+        scoreText.text = "Score: " + score;
     }
 
     public int getScore()
